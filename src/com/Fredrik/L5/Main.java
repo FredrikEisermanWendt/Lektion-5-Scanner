ddbb5bff7cfce030e1aa9ab6fe32b4489c90fec5
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-09-19 13:16:58 +0200

    Second commit

--- src/com/Fredrik/L5/Main.java
+++ src/com/Fredrik/L5/Main.java
@@ -1,11 +1,41 @@
 package com.Fredrik.L5;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Let's scan");
+        boolean isPlaying = true;
 
+        do{
+            int result = scanInt();
+
+            for (int i = 0; i < result; i++){
+                System.out.println(i + 1);
+                System.out.println("Looping...");
+            }
+
+            if (result > 1000){
+                isPlaying = false;
+            }
+
+        }while(isPlaying);
 
 
     }
+
+    public static String scanText(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insert some words:");
+        String input = scan.nextLine();
+        return input;
+    }
+
+    public static int scanInt(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+        return scan.nextInt();
+    }
+
 }
