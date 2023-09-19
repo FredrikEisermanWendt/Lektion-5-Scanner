ab81ea11671a2d56ea0facd1e210669d28aa3439
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-09-19 13:31:08 +0200

    basic scanner ex

--- src/com/Fredrik/L5/Main.java
+++ src/com/Fredrik/L5/Main.java
@@ -4,38 +4,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        boolean isPlaying = true;
-
-        do{
-            int result = scanInt();
-
-            for (int i = 0; i < result; i++){
-                System.out.println(i + 1);
-                System.out.println("Looping...");
-            }
-
-            if (result > 1000){
-                isPlaying = false;
-            }
-
-        }while(isPlaying);
-
-
-    }
-
-    public static String scanText(){
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Insert some words:");
-        String input = scan.nextLine();
-        return input;
+        do {
+
+            System.out.println("Input number");
+            int result = scan.nextInt();
+
+            System.out.println("You picked " + result);
+
+        }while(true);
+
+
+
+
     }
 
-    public static int scanInt(){
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Please insert a number");
-        return scan.nextInt();
-    }
 
 }
